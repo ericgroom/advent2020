0398232557812ad40381230416e177539d5567e7
Eric Groom <bcgroom@gmail.com>
2020-12-01 18:22:36 -0800

    add tests for real inputs

--- test/days/day_1_test.exs
+++ test/days/day_1_test.exs
@@ -9,6 +9,10 @@ defmodule Advent2020.Days.Day1Test do
       output = find_expense_anomoly(input)
       assert output == 514579
     end
+
+    test "real input" do
+      assert part_one() == 876459
+    end
   end
 
   describe "find_other_anomoly" do
@@ -16,6 +20,10 @@ defmodule Advent2020.Days.Day1Test do
       input = [1721, 979, 366, 299, 675, 1456]
       output = find_other_anomoly(input)
       assert output == 241861950
+    end
+
+    test "real input" do
+      assert part_two() == 116168640
     end
   end
 end
