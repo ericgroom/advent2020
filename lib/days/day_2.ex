fa5bdfd10ac867b394715c8fd787a3345c70a9e5
Eric Groom <bcgroom@gmail.com>
2020-12-02 09:09:44 -0800

    day2 part2

--- lib/days/day_2.ex
+++ lib/days/day_2.ex
@@ -2,16 +2,34 @@ defmodule Advent2020.Days.Day2 do
   use Advent2020.Day, day: 2, input: "day_2_input.txt"
 
   def part_one do
+    count_valid_passwords(&valid_password?/2)
+  end
+
+  def part_two do
+    count_valid_passwords(&valid_official_password?/2)
+  end
+
+  defp count_valid_passwords(validator) when is_function(validator, 2) do
     @input
     |> parse_passwords()
-    |> Enum.count(fn {password, policy} -> valid_password?(password, policy) end)
+    |> Enum.count(fn {password, policy} -> validator.(password, policy) end)
   end
-  def part_two, do: nil
 
-  def valid_password?(password, policy) do
+  alias __MODULE__.PasswordPolicy
+  defmodule __MODULE__.PasswordPolicy do
+    defstruct [:num_one, :num_two, :letter]
+  end
+
+  def valid_official_password?(password, %PasswordPolicy{num_one: pos_one, num_two: pos_two, letter: letter}) do
+    letter_one = String.at(password, pos_one-1)
+    letter_two = String.at(password, pos_two-1)
+    (letter_one == letter and letter_two != letter) or (letter_two == letter and letter_one != letter)
+  end
+
+  def valid_password?(password, %PasswordPolicy{num_one: min, num_two: max, letter: letter}) do
     letter_frequencies = count_letters(password)
-    actual_count = Map.get(letter_frequencies, policy.letter, 0)
-    policy.min <= actual_count and actual_count <= policy.max
+    actual_count = Map.get(letter_frequencies, letter, 0)
+    min <= actual_count and actual_count <= max
   end
 
   defp count_letters(word) do
@@ -20,11 +38,6 @@ defmodule Advent2020.Days.Day2 do
     |> Enum.reduce(%{}, fn letter, acc ->
       Map.update(acc, letter, 1, fn count -> count + 1 end)
     end)
-  end
-
-  alias __MODULE__.PasswordPolicy
-  defmodule __MODULE__.PasswordPolicy do
-    defstruct [:min, :max, :letter]
   end
 
   defp parse_passwords raw do
@@ -44,6 +57,6 @@ defmodule Advent2020.Days.Day2 do
   defp parse_policy policy do
     [range, letter] = String.split(policy, " ")
     [min, max] = String.split(range, "-") |> Enum.map(&String.to_integer/1)
-    %PasswordPolicy{min: min, max: max, letter: letter}
+    %PasswordPolicy{num_one: min, num_two: max, letter: letter}
   end
 end
--- test/days/day_2_test.exs
+++ test/days/day_2_test.exs
@@ -6,13 +6,25 @@ defmodule Advent2020.Days.Day2Test do
 
   describe "valid_password?" do
     test "sample input" do
-      assert valid_password?("abcde", %PasswordPolicy{min: 1, max: 3, letter: "a"})
-      refute valid_password?("cdefg", %PasswordPolicy{min: 1, max: 3, letter: "b"})
-      assert valid_password?("ccccccccc", %PasswordPolicy{min: 2, max: 9, letter: "c"})
+      assert valid_password?("abcde", %PasswordPolicy{num_one: 1, num_two: 3, letter: "a"})
+      refute valid_password?("cdefg", %PasswordPolicy{num_one: 1, num_two: 3, letter: "b"})
+      assert valid_password?("ccccccccc", %PasswordPolicy{num_one: 2, num_two: 9, letter: "c"})
     end
 
     test "real input" do
       assert part_one() == 422
+    end
+  end
+
+  describe "valid_official_password?" do
+    test "sample input" do
+      assert valid_official_password?("abcde", %PasswordPolicy{num_one: 1, num_two: 3, letter: "a"})
+      refute valid_official_password?("cdefg", %PasswordPolicy{num_one: 1, num_two: 3, letter: "b"})
+      refute valid_official_password?("ccccccccc", %PasswordPolicy{num_one: 2, num_two: 9, letter: "c"})
+    end
+
+    test "real input" do
+      assert part_two() == 451
     end
   end
 end
