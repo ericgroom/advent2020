bb6318db67cd0a016949e2c695894159d6ab9fc5
Eric Groom <bcgroom@gmail.com>
2020-12-01 18:20:54 -0800

    day 1 cleanup

--- lib/days/day_1.ex
+++ lib/days/day_1.ex
@@ -17,7 +17,7 @@ defmodule Advent2020.Days.Day1 do
   @anomoly_sum 2020
 
   def find_expense_anomoly items do
-    pairs = permutations_exluding_self(items)
+    pairs = permutations_excluding_self(items)
 
     {expense_one, expense_two} = Enum.find(pairs, fn {x, y} ->
       x + y == @anomoly_sum
@@ -28,7 +28,7 @@ defmodule Advent2020.Days.Day1 do
 
   def find_other_anomoly items do
     items_set = MapSet.new(items)
-    pairs = permutations_exluding_self(items)
+    pairs = permutations_excluding_self(items)
 
     {expense_one, expense_two} = Enum.find(pairs, fn {x, y} ->
       counterpart = @anomoly_sum - x - y
@@ -39,14 +39,14 @@ defmodule Advent2020.Days.Day1 do
     expense_one * expense_two * expense_three
   end
 
-  defp permutations_exluding_self items do
-    enumerated_items = Enum.with_index(items)
+  defp permutations_excluding_self items do
+    enumerated_items = Stream.with_index(items)
     enumerated_items
-    |> Enum.flat_map(fn {item, index} ->
-      Enum.map(enumerated_items, fn {second_item, second_index} ->
+    |> Stream.flat_map(fn {item, index} ->
+      Stream.map(enumerated_items, fn {second_item, second_index} ->
         if index == second_index, do: nil, else: {item, second_item}
       end)
     end)
-    |> Enum.filter(fn item -> item != nil end)
+    |> Stream.filter(fn item -> item != nil end)
   end
 end
