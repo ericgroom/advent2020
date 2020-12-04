53babfeae7b51417f3af6a905f95e6bdceeae67a
Eric Groom <bcgroom@gmail.com>
2020-12-04 09:00:28 -0800

    day4 part2

--- lib/days/day_4.ex
+++ lib/days/day_4.ex
@@ -9,6 +9,16 @@ defmodule Advent2020.Days.Day4 do
     |> Enum.count()
   end
 
+  def part_two do
+    @input
+    |> parse()
+    |> Enum.map(fn passport ->
+      valid_north_pole_credentials?(passport) && valid_values?(passport)
+    end)
+    |> Enum.filter(& &1)
+    |> Enum.count()
+  end
+
   defp parse(raw) do
     raw
     |> String.split("\n\n")
@@ -35,4 +45,50 @@ defmodule Advent2020.Days.Day4 do
     |> Enum.map(&Map.fetch(passport, &1))
     |> Enum.all?(fn field -> match?({:ok, _value}, field) end)
   end
+
+  def valid_values?(passport) do
+    @north_pole_fields
+    |> Enum.map(fn key -> {key, Map.fetch!(passport, key)} end)
+    |> Enum.all?(fn {key, value} -> valid_key_value?(key, value) end)
+  end
+
+  def valid_key_value?("byr", year) do
+    int_year = String.to_integer(year)
+    String.length(year) == 4 and 1920 <= int_year and int_year <= 2002
+  end
+
+  def valid_key_value?("iyr", year) do
+    int_year = String.to_integer(year)
+    String.length(year) == 4 and 2010 <= int_year and int_year <= 2020
+  end
+
+  def valid_key_value?("eyr", year) do
+    int_year = String.to_integer(year)
+    String.length(year) == 4 and 2020 <= int_year and int_year <= 2030
+  end
+
+  def valid_key_value?("hgt", height) do
+    {int, unit} = Integer.parse(height)
+
+    case unit do
+      "cm" -> 150 <= int and int <= 193
+      "in" -> 59 <= int and int <= 76
+      _ -> false
+    end
+  end
+
+  def valid_key_value?("hcl", color) do
+    String.length(color) == 7 && String.starts_with?(color, "#") &&
+      match?({_int, ""}, Integer.parse(String.slice(color, 1..String.length(color)), 16))
+  end
+
+  def valid_key_value?("ecl", color) do
+    Enum.member?(["amb", "blu", "brn", "gry", "grn", "hzl", "oth"], color)
+  end
+
+  def valid_key_value?("pid", pid) do
+    String.length(pid) == 9 and match?({_int, ""}, Integer.parse(pid, 10))
+  end
+
+  def valid_key_value?("cid", _cid), do: true
 end
--- test/days/day_4_test.exs
+++ test/days/day_4_test.exs
@@ -47,7 +47,57 @@ defmodule Advent2020.Days.Day4Test do
     end
 
     test "real input" do
-      assert part_one == 250
+      assert part_one() == 250
+    end
+  end
+
+  describe "valid_key_value/2" do
+    test "sample inputs" do
+      assert valid_key_value?("byr", "2002")
+      refute valid_key_value?("byr", "2003")
+
+      assert valid_key_value?("hgt", "60in")
+      assert valid_key_value?("hgt", "190cm")
+      refute valid_key_value?("hgt", "190in")
+      refute valid_key_value?("hgt", "190")
+
+      assert valid_key_value?("hcl", "#123abc")
+      refute valid_key_value?("hcl", "#123abz")
+      refute valid_key_value?("hcl", "123abc")
+
+      assert valid_key_value?("ecl", "brn")
+      refute valid_key_value?("ecl", "wat")
+
+      assert valid_key_value?("pid", "000000001")
+      refute valid_key_value?("pid", "0123456789")
+    end
+  end
+
+  describe "valid_values?/1" do
+    test "sample inputs" do
+      assert valid_values?(%{
+               "ecl" => "grn",
+               "pid" => "087499704",
+               "eyr" => "2030",
+               "hcl" => "#623a2f",
+               "byr" => "1980",
+               "iyr" => "2012",
+               "hgt" => "74in"
+             })
+
+      refute valid_values?(%{
+               "ecl" => "amb",
+               "pid" => "186cm",
+               "eyr" => "1972",
+               "hcl" => "#18171d",
+               "byr" => "1926",
+               "iyr" => "2018",
+               "hgt" => "170"
+             })
+    end
+
+    test "real input" do
+      assert part_two() == 158
     end
   end
 end
