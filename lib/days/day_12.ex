749c71c0da3db1d281b3b7a83641c5b66a40891e
Eric Groom <bcgroom@gmail.com>
2020-12-12 09:20:35 -0800

    day12 part1

---
+++ lib/days/day_12.ex
@@ -0,0 +1,67 @@
+defmodule Advent2020.Days.Day12 do
+  use Advent2020.Day, day: 12
+
+  # following mathematical coordinates y+ north y- south x+ east x- west
+
+  def part_one do
+    @input
+    |> parse()
+    |> follow_navigation()
+    |> distance_from_start()
+  end
+
+  def parse(raw) do
+    raw
+    |> String.split("\n", trim: true)
+    |> Enum.map(&String.split_at(&1, 1))
+    |> Enum.map(&parse_instruction/1)
+  end
+
+  def distance_from_start({x, y, _heading}) do
+    abs(x) + abs(y)
+  end
+
+  def follow_navigation(chart) do
+    Enum.reduce(chart, {0, 0, :east}, fn adjustment, pos ->
+      adjust_ship_position(pos, adjustment)
+    end)
+  end
+
+  defp parse_instruction({"N", amount}), do: {:north, String.to_integer(amount)}
+  defp parse_instruction({"S", amount}), do: {:south, String.to_integer(amount)}
+  defp parse_instruction({"E", amount}), do: {:east, String.to_integer(amount)}
+  defp parse_instruction({"W", amount}), do: {:west, String.to_integer(amount)}
+  defp parse_instruction({"F", amount}), do: {:forward, String.to_integer(amount)}
+  defp parse_instruction({"L", amount}), do: {:left, String.to_integer(amount)}
+  defp parse_instruction({"R", amount}), do: {:right, String.to_integer(amount)}
+
+  defp adjust_ship_position({x, y, heading}, {:north, amount}), do: {x, y + amount, heading}
+  defp adjust_ship_position({x, y, heading}, {:south, amount}), do: {x, y - amount, heading}
+  defp adjust_ship_position({x, y, heading}, {:east, amount}), do: {x - amount, y, heading}
+  defp adjust_ship_position({x, y, heading}, {:west, amount}), do: {x + amount, y, heading}
+  defp adjust_ship_position({x, y, heading}, {:forward, amount}), do: adjust_ship_position({x, y, heading}, {heading, amount})
+  defp adjust_ship_position({x, y, heading}, {:left = dir, degrees}), do: {x, y, rotate(heading, degrees, dir)}
+  defp adjust_ship_position({x, y, heading}, {:right = dir, degrees}), do: {x, y, rotate(heading, degrees, dir)}
+
+  defp rotate(heading, degrees, direction) do
+    encoded_deg = encode_degrees(degrees) * sign(direction)
+    encoded_heading = encode_heading(heading)
+    new_heading = rem(encoded_heading + encoded_deg + 4, 4)
+    decode_heading(new_heading)
+  end
+
+  defp sign(:left), do: -1
+  defp sign(:right), do: 1
+
+  defp encode_degrees(degrees), do: div(degrees, 90)
+
+  defp encode_heading(:north), do: 0
+  defp encode_heading(:east), do: 1
+  defp encode_heading(:south), do: 2
+  defp encode_heading(:west), do: 3
+
+  defp decode_heading(0), do: :north
+  defp decode_heading(1), do: :east
+  defp decode_heading(2), do: :south
+  defp decode_heading(3), do: :west
+end
---
+++ test/days/day_12_test.exs
@@ -0,0 +1,34 @@
+defmodule Advent2020.Days.Day12Test do
+  use ExUnit.Case, async: true
+
+  import Advent2020.Days.Day12
+
+  @sample [
+    {:forward, 10},
+    {:north, 3},
+    {:forward, 7},
+    {:right, 90},
+    {:forward, 11}
+  ]
+
+  describe "follow_navigation/1" do
+    test "sample input" do
+      assert follow_navigation(@sample) |> distance_from_start() == 25
+    end
+
+    test "rotation" do
+      to_north = [{:left, 90}]
+      assert follow_navigation(to_north) == {0, 0, :north}
+
+      to_south_left = [{:left, 270}]
+      assert follow_navigation(to_south_left) == {0, 0, :south}
+
+      to_south_right = [{:right, 90}]
+      assert follow_navigation(to_south_right) == {0, 0, :south}
+    end
+
+    test "real input" do
+      assert part_one() == 2879
+    end
+  end
+end
