720727db9662fa9ed74a2b69c8b8a8b568a86033
Eric Groom <bcgroom@gmail.com>
2020-12-13 13:00:37 -0800

    day13 part2

--- lib/days/day_13.ex
+++ lib/days/day_13.ex
@@ -2,23 +2,66 @@ defmodule Advent2020.Days.Day13 do
   use Advent2020.Day, day: 13
 
   def part_one do
-    {earliest, busses} = parse(@input)
-    without_xs = Enum.filter(busses, &is_integer/1)
+    {earliest, buses} = parse(@input)
+    without_xs = Enum.filter(buses, &is_integer/1)
     {bus_id, departs} = find_earliest_bus(earliest, without_xs)
     wait_time = departs - earliest
     bus_id * wait_time
   end
 
+  def part_two do
+    {_earliest, buses} = parse(@input)
+    win_contest(buses)
+  end
+
+  def win_contest(buses) do
+    with_rem = buses
+    |> Stream.with_index()
+    |> Stream.filter(fn {v, _i} -> v != "x" end)
+    |> invert_rem()
+
+    big_n = calc_N(with_rem)
+
+    unreduced_solution = with_rem
+    |> Enum.map(&calc_row_product(&1, big_n))
+    |> Enum.sum()
+
+    rem(unreduced_solution, big_n)
+  end
+
+  def calc_row_product({id, rem}, big_n) do
+    n_i = div(big_n, id)
+    # n_i*x_i cong. 1 (mod id)
+    reduced_n_i = rem(n_i, id)
+    x_i = Enum.find(integers(), fn x_i ->
+      rem(reduced_n_i*x_i, id) == 1
+    end)
+    x_i * n_i * rem
+  end
+
+  def integers(), do: 1..10000
+
+  def invert_rem(buses) do
+    buses
+    |> Enum.map(fn {id, rem} -> {id, rem((id - rem), id)} end)
+  end
+
+  def calc_N(buses) do
+    buses
+    |> Enum.map(fn {id, _rem} -> id end)
+    |> Enum.reduce(1, &*/2)
+  end
+
   def parse(raw) do
-    [earliest, busses] = String.split(raw, "\n", trim: true)
+    [earliest, buses] = String.split(raw, "\n", trim: true)
     earliest = String.to_integer(earliest)
-    busses = busses
+    buses = buses
         |> String.split(",")
         |> Enum.map(fn
             "x" -> "x"
              n -> String.to_integer(n)
         end)
-    {earliest, busses}
+    {earliest, buses}
   end
 
   def find_earliest_bus(after_time, bus_ids) do
--- test/days/day_13_test.exs
+++ test/days/day_13_test.exs
@@ -12,4 +12,19 @@ defmodule Advent2020.Days.Day13Test do
       assert part_one() == 2545
     end
   end
+
+  describe "win_contest/1" do
+    test "sample inputs" do
+      assert win_contest([17,"x",13,19]) == 3417
+      assert win_contest([7,13,"x","x",59,"x",31,19]) == 1068781
+      assert win_contest([67,7,59,61]) == 754018
+      assert win_contest([67,"x",7,59,61]) == 779210
+      assert win_contest([67,7,"x",59,61]) == 1261476
+      assert win_contest([1789,37,47,1889]) == 1202161486
+    end
+
+    test "real input" do
+      assert part_two() == 266204454441577
+    end
+  end
 end
