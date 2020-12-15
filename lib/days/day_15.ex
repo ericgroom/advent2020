936a6d760f5497750abd7c56e2af2c520bc18b98
Eric Groom <bcgroom@gmail.com>
2020-12-15 08:19:43 -0800

    pass in target turn

--- lib/days/day_15.ex
+++ lib/days/day_15.ex
@@ -4,26 +4,26 @@ defmodule Advent2020.Days.Day15 do
   @input [16, 1, 0, 18, 12, 14, 19]
 
   def part_one do
-    play_memory_game(@input)
+    play_memory_game(@input, 2020)
   end
 
-  def play_memory_game(starting_numbers) do
+  def play_memory_game(starting_numbers, target) do
     memory = starting_numbers
       |> Stream.with_index(1)
       |> Enum.into(%{})
     turn = map_size(memory) + 1
     previously_spoken = List.last(starting_numbers)
-    play_turns(memory, turn, previously_spoken)
+    play_turns(memory, turn, previously_spoken, target)
   end
 
-  defp play_turns(_memory, 2021, previously_spoken), do: previously_spoken
-  defp play_turns(memory, turn, previously_spoken) do
+  defp play_turns(_memory, turn, previously_spoken, target) when turn == target + 1, do: previously_spoken
+  defp play_turns(memory, turn, previously_spoken, target) do
     previous_turn = turn - 1
     new_number = case Map.fetch(memory, previously_spoken) do
       {:ok, spoken_on} -> previous_turn - spoken_on
       :error -> 0
     end
     new_memory = Map.put(memory, previously_spoken, previous_turn)
-    play_turns(new_memory, turn + 1, new_number)
+    play_turns(new_memory, turn + 1, new_number, target)
   end
 end
--- test/days/day_15_test.exs
+++ test/days/day_15_test.exs
@@ -5,13 +5,13 @@ defmodule Advent2020.Days.Day15Test do
 
   describe "play_memory_game/1" do
     test "sample inputs" do
-      assert play_memory_game([0, 3, 6]) == 436
-      assert play_memory_game([1, 3, 2]) == 1
-      assert play_memory_game([2, 1, 3]) == 10
-      assert play_memory_game([1, 2, 3]) == 27
-      assert play_memory_game([2, 3, 1]) == 78
-      assert play_memory_game([3, 2, 1]) == 438
-      assert play_memory_game([3, 1, 2]) == 1836
+      assert play_memory_game([0, 3, 6], 2020) == 436
+      assert play_memory_game([1, 3, 2], 2020) == 1
+      assert play_memory_game([2, 1, 3], 2020) == 10
+      assert play_memory_game([1, 2, 3], 2020) == 27
+      assert play_memory_game([2, 3, 1], 2020) == 78
+      assert play_memory_game([3, 2, 1], 2020) == 438
+      assert play_memory_game([3, 1, 2], 2020) == 1836
     end
 
     test "real input" do
