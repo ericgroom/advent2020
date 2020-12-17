bb2d0279836fb83310edaf719b67a5b7f782bd9b
Eric Groom <bcgroom@gmail.com>
2020-12-17 09:04:41 -0800

    day17 part1

--- lib/days/day_17.ex
+++ lib/days/day_17.ex
@@ -2,4 +2,71 @@ defmodule Advent2020.Days.Day17 do
   use Advent2020.Day, day: 17
 
   alias Advent2020.DataStructures.Vec3D
+
+  def part_one do
+    @input
+    |> parse()
+    |> run_n_cycles(6)
+    |> count_active()
+  end
+
+  defp count_active(grid) do
+    grid
+    |> Map.values()
+    |> Enum.count(& &1 == :active)
+  end
+
+  def run_n_cycles(grid, n) do
+    Stream.iterate(grid, &run_cycle/1)
+    |> Enum.at(n)
+  end
+
+  def run_cycle(grid) do
+    with_immediate_neighbors = grid
+      |> Map.keys()
+      |> Enum.reduce(grid, fn coord, acc ->
+        Vec3D.unit_vectors()
+        |> Enum.map(&Vec3D.add(&1, coord))
+        |> Enum.reduce(acc, &Map.put_new(&2, &1, :inactive))
+      end)
+
+    with_immediate_neighbors
+    |> Map.keys()
+    |> Enum.into(%{}, fn coord ->
+      neighbor_coords = Vec3D.unit_vectors() |> Enum.map(&Vec3D.add(&1, coord))
+      neighbors = Enum.map(neighbor_coords, fn coord -> {coord, Map.get(with_immediate_neighbors, coord, :inactive)} end)
+      # TODO don't forget to add neighbors to grid
+      {coord, determine_state(with_immediate_neighbors, coord, neighbors)}
+    end)
+  end
+
+  defp determine_state(grid, at, neighbors) do
+    current = Map.fetch!(grid, at)
+    active_count = neighbors
+      |> Enum.count(fn {_coord, val} -> val == :active end)
+
+    cond do
+      current == :active and (active_count == 2 or active_count == 3)  ->
+        :active
+      current == :inactive and active_count == 3 ->
+        :active
+      true ->
+        :inactive
+    end
+  end
+
+  def parse(raw) do
+    Parser.parse_grid(raw, fn
+      "." -> :inactive
+      "#" -> :active
+    end)
+    |> Enum.with_index()
+    |> Enum.flat_map(fn {row, y} ->
+      Enum.with_index(row)
+      |> Enum.map(fn {elem, x} ->
+        {{x, y, 0}, elem}
+      end)
+    end)
+    |> Enum.into(%{})
+  end
 end
--- test/days/day_17_test.exs
+++ test/days/day_17_test.exs
@@ -1,6 +1,50 @@
 defmodule Advent2020.Days.Day17Test do
   use ExUnit.Case, async: true
 
+  import Advent2020.Days.Day17
+
+  @sample """
+  .#.
+  ..#
+  ###
+  """
+
+  describe "parse/1" do
+    test "can parse sample" do
+      grid = parse(@sample)
+      assert map_size(grid) == 9
+      assert grid[{0, 0, 0}] == :inactive
+      assert grid[{2, 2, 0}] == :active
+    end
+  end
+
+  describe "run_cycle/1" do
+    test "one cycle, sample input" do
+      input = parse(@sample)
+      output = run_n_cycles(input, 1)
+      active_count = Map.values(output) |> Enum.count(& &1 == :active)
+      assert active_count == 11
+    end
+
+    test "two cycles, sample input" do
+      input = parse(@sample)
+      output = run_n_cycles(input, 2)
+      active_count = Map.values(output) |> Enum.count(& &1 == :active)
+      assert active_count == 21
+    end
+
+    test "six cycled, sample input" do
+      input = parse(@sample)
+      output = run_n_cycles(input, 6)
+      active_count = Map.values(output) |> Enum.count(& &1 == :active)
+      assert active_count == 112
+    end
+
+    test "real input" do
+      assert part_one() == 306
+    end
+  end
+
   describe "Vec3D.unit_vectors/0" do
     alias Advent2020.DataStructures.Vec3D
 
