39819bbef42a582a03c41680df07335642f50101
Eric Groom <bcgroom@gmail.com>
2020-12-18 08:58:17 -0800

    working lexer, parser and evaluator

--- lib/days/day_18.ex
+++ lib/days/day_18.ex
@@ -1,16 +1,52 @@
 defmodule Advent2020.Days.Day18 do
   use Advent2020.Day, day: 18
 
-  # stack based aside from parens
-  # could be represented as nested lists?
-  # how to represent operators?
-  # tree of tuples
-  # {1, "*", 2}
-  # {1, "+", {{2, "*", 3}}}
-  # follow elixir
-  # {:+, [1, {:*, [2, 3]}]}
+  def parse_expr(raw) do
+    raw
+    |> String.graphemes()
+    |> Stream.map(fn
+      " " -> nil
+      "+" -> :+
+      "*" -> :*
+      "(" -> :open
+      ")" -> :close
+      x -> int!(x) # input only seems to be single digits, probably for easier parsing
+    end)
+    |> Enum.filter(fn x -> not is_nil(x) end)
+  end
+
+  def build_ast([x]) when is_tuple(x), do: x
+  def build_ast(tokens) do
+    # take two "operands", build a tree, nest that tree in the next expression
+    {first_operand, rest} = take_operand(tokens)
+    {op, rest} = take_operator(rest)
+    {second_operand, rest} = take_operand(rest)
+    expr = {op, first_operand, second_operand}
+    build_ast([expr | rest])
+  end
+
+  defp take_operand([x | rest]) when is_integer(x), do: {x, rest}
+  defp take_operand([x | rest]) when x == :open do
+    {in_parens, rest} = take_parens([x | rest])
+    {build_ast(in_parens), rest}
+  end
+  defp take_operand([x | rest]) when is_tuple(x), do: {x, rest}
+  defp take_operator([:+ | rest]), do: {:+, rest}
+  defp take_operator([:* | rest]), do: {:*, rest}
+
+  defp take_parens(tokens, so_far \\ [], counter \\ 0)
+  defp take_parens([:close | rest], so_far, 1), do: {so_far, rest}
+  defp take_parens([:close | rest], so_far, x), do: take_parens(rest, so_far ++ [:close], x-1)
+  defp take_parens([:open | rest], so_far, 0), do: take_parens(rest, so_far, 1)
+  defp take_parens([:open | rest], so_far, x), do: take_parens(rest, so_far ++ [:open], x+1)
+  defp take_parens([x | rest], so_far, counter), do: take_parens(rest, so_far ++ [x], counter)
 
   def eval(x) when is_integer(x), do: x
-  def eval({:+, args}), do: Enum.map(args, &eval/1) |> Enum.reduce(0, &+/2)
-  def eval({:*, args}), do: Enum.map(args, &eval/1) |> Enum.reduce(1, &*/2)
+  def eval({:+, x, y}), do: eval(x) + eval(y)
+  def eval({:*, x, y}), do: eval(x) * eval(y)
+
+  defp int!(x) do
+    {x, ""} = Integer.parse(x)
+    x
+  end
 end
--- test/days/day_18_test.exs
+++ test/days/day_18_test.exs
@@ -6,28 +6,58 @@ defmodule Advent2020.Days.Day18Test do
   describe "eval/1" do
     test "manual AST sanity check" do
       # 1 + 2
-      ast = {:+, [1, 2]}
+      ast = {:+, 1, 2}
       assert eval(ast) == 3
     end
 
     test "manual AST larger sanity check" do
       # 1 + 2 * 3 + 4 * 5 + 6
-      first = {:+, [1, 2]}
-      second = {:*, [first, 3]}
-      third = {:+, [second, 4]}
-      fourth = {:*, [third, 5]}
-      fifth = {:+, [fourth, 6]}
+      first = {:+, 1, 2}
+      second = {:*, first, 3}
+      third = {:+, second, 4}
+      fourth = {:*, third, 5}
+      fifth = {:+, fourth, 6}
       assert eval(fifth) == 71
     end
 
     test "manual AST parens" do
       # 1 + (2 * 3) + (4 * (5 + 6))
-      first = {:+, [5, 6]}
-      second = {:*, [4, first]}
-      third = {:*, [2, 3]}
-      fourth = {:+, [1, third]}
-      fifth = {:+, [fourth, second]}
+      first = {:+, 5, 6}
+      second = {:*, 4, first}
+      third = {:*, 2, 3}
+      fourth = {:+, 1, third}
+      fifth = {:+, fourth, second}
       assert eval(fifth) == 51
+    end
+  end
+
+  describe "build_ast/1" do
+    test "manual sanity check" do
+      tokens = [1, :+, 2]
+      assert build_ast(tokens) == {:+, 1, 2}
+    end
+
+    test "larger sanity check" do
+      tokens = [1, :+, 2, :*, 3]
+      assert build_ast(tokens) == {:*, {:+, 1, 2}, 3}
+    end
+
+    test "parens sanity check" do
+      tokens = [1, :+, :open, 2, :*, 3, :close, :+, :open, 4, :*, :open, 5, :+, 6, :close, :close]
+      first = {:+, 5, 6}
+      second = {:*, 4, first}
+      third = {:*, 2, 3}
+      fourth = {:+, 1, third}
+      expected_ast = {:+, fourth, second}
+      assert build_ast(tokens) == expected_ast
+    end
+  end
+
+  describe "parse_expr/1" do
+    test "sanity check" do
+      raw = "1 + (2 * 3) + (4 * (5 + 6))"
+      tokens = [1, :+, :open, 2, :*, 3, :close, :+, :open, 4, :*, :open, 5, :+, 6, :close, :close]
+      assert parse_expr(raw) == tokens
     end
   end
 end
