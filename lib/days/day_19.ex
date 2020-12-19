150939ede1de26bacadd228d2d17750a77e1d28c
Eric Groom <bcgroom@gmail.com>
2020-12-19 11:29:00 -0800

    day19 part2

--- lib/days/day_19.ex
+++ lib/days/day_19.ex
@@ -8,6 +8,55 @@ defmodule Advent2020.Days.Day19 do
     |> Enum.count()
   end
 
+  def part_two do
+    # instead of a list create a stream, zip with the inputs?
+    # 8 essentially means 42 as many times as you want
+    # 11 essentially means nest 42 31 into itself as many as you want
+    # NOTHING USES 8 and 11 but 0
+    {rules, messages} = parse(@input)
+    rules = rules |> Enum.into(%{})
+    find_matches_pt2(messages, rules)
+    |> Enum.count()
+  end
+
+  def find_matches_pt2(messages, rules) do
+    # 0: 8 11
+    # 8: 42 | 42 8
+    # 11: 42 31 | 42 11 31
+    # take from 8 until it doesn't match?
+    # enumerate all pairs up until the longest message?
+    # iterate in reverse until 31 no longer matches
+    # make sure the rest are 42 and that there are as least as many as 31
+    forty_two_patterns = resolve_rule(42, rules) |> MapSet.new()
+    thirty_one_patterns = resolve_rule(31, rules) |> MapSet.new()
+    chunk_size = thirty_one_patterns |> Enum.take(1) |> List.first() |> String.length()
+
+    messages
+    |> Enum.filter(fn message ->
+      {thirty_one_matches, rest} = message
+        |> String.graphemes()
+        |> Enum.chunk_every(chunk_size)
+        |> Enum.map(fn letters ->
+          Enum.join(letters)
+        end)
+        |> Enum.reverse()
+        |> Enum.split_while(fn chunk -> MapSet.member?(thirty_one_patterns, chunk) end)
+
+      forty_two_matches_count = Enum.count(rest, fn chunk -> MapSet.member?(forty_two_patterns, chunk) end)
+      thirty_one_matches_count = Enum.count(thirty_one_matches)
+      ratio_is_good = (forty_two_matches_count - 1) >= thirty_one_matches_count # asserts that there is at least one more 42 than there is 31
+      matched_full_string = ((forty_two_matches_count * chunk_size) + (thirty_one_matches_count * chunk_size)) == String.length(message)
+      # IO.puts "thirty_one: #{thirty_one_matches_count}"
+      # IO.puts "forty_two: #{forty_two_matches_count}"
+      # IO.puts "matched full: #{matched_full_string}"
+      # IO.puts "good ratio: #{ratio_is_good}"
+
+      thirty_one_matches_count > 0
+      and ratio_is_good
+      and matched_full_string
+    end)
+  end
+
   def find_matches(messages, rules, rule_no) do
     patterns = resolve_rule(rule_no, rules) |> MapSet.new()
 
